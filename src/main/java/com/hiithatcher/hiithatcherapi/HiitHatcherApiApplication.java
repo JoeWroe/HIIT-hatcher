ad5204fcf9bf78ad39c4be12d59cb272ffdc8816
Joe Wroe <joe_wroe@icloud.com>
2020-06-06 21:28:36 +0100

    Spring initializr
    JW

---
+++ src/main/java/com/hiithatcher/hiithatcherapi/HiitHatcherApiApplication.java
@@ -0,0 +1,13 @@
+package com.hiithatcher.hiithatcherapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HiitHatcherApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HiitHatcherApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hiithatcher/hiithatcherapi/HiitHatcherApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hiithatcher.hiithatcherapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HiitHatcherApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
