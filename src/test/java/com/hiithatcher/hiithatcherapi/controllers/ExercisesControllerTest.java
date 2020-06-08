4352d01631b4d677cbe4eae681295879ca19e3d6
Joe Wroe <joe_wroe@icloud.com>
2020-06-08 23:29:40 +0100

    Unit test ExerciseController "/api/exercises"
    JW

--- src/test/java/com/hiithatcher/hiithatcherapi/controllers/ExercisesControllerTest.java
+++ src/test/java/com/hiithatcher/hiithatcherapi/controllers/ExercisesControllerTest.java
@@ -2,6 +2,7 @@ package com.hiithatcher.hiithatcherapi.controllers;
 
 import com.hiithatcher.hiithatcherapi.models.Exercise;
 import com.hiithatcher.hiithatcherapi.repositories.ExercisesRepository;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +12,9 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.util.List;
+
+import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.Matchers.is;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -27,12 +31,34 @@ class ExercisesControllerTest {
     @MockBean
     private ExercisesRepository repository;
 
-    @Test
-    void shouldReturnAnExerciseWithTheGivenName() throws Exception {
-        Exercise crunch = Exercise.builder()
+    Exercise crunch;
+    Exercise squat;
+
+    @BeforeEach
+    void setUp() {
+        crunch = Exercise.builder()
             .name("crunch")
             .build();
 
+        squat = Exercise.builder()
+            .name("squat")
+            .build();
+    }
+
+    @Test
+    void shouldReturnAllExercises() throws Exception {
+        when(repository.findAll()).thenReturn(List.of(crunch, squat));
+
+        mvc.perform(get("/api/exercises/")
+        .contentType(MediaType.APPLICATION_JSON))
+            .andExpect(status().isOk())
+            .andExpect(jsonPath("$", hasSize(2)))
+            .andExpect(jsonPath("$[0].name", is("crunch")))
+            .andExpect(jsonPath("$[1].name", is("squat")));
+    }
+
+    @Test
+    void shouldReturnAnExerciseWithTheGivenName() throws Exception {
         when(repository.findByName("crunch")).thenReturn(crunch);
 
         mvc.perform(get("/api/exercises/crunch")
