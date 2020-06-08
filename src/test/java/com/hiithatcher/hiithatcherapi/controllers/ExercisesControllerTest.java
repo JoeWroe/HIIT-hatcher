ee967c083a11cf66f53070198e5a9af89a63770f
Joe Wroe <joe_wroe@icloud.com>
2020-06-08 22:04:20 +0100

    Refactor controller test
    ∆ ...to work
    
    JW

--- src/test/java/com/hiithatcher/hiithatcherapi/controllers/ExercisesControllerTest.java
+++ src/test/java/com/hiithatcher/hiithatcherapi/controllers/ExercisesControllerTest.java
@@ -1,14 +1,18 @@
 package com.hiithatcher.hiithatcherapi.controllers;
 
+import com.hiithatcher.hiithatcherapi.models.Exercise;
+import com.hiithatcher.hiithatcherapi.repositories.ExercisesRepository;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.hamcrest.Matchers.*;
+import static org.hamcrest.Matchers.is;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -20,11 +24,20 @@ class ExercisesControllerTest {
     @Autowired
     private MockMvc mvc;
 
+    @MockBean
+    private ExercisesRepository repository;
+
     @Test
     void shouldReturnAnExerciseWithTheGivenName() throws Exception {
-        mvc.perform(get("/api/exercises/push-up")
+        Exercise crunch = Exercise.builder()
+            .name("crunch")
+            .build();
+
+        when(repository.findByName("crunch")).thenReturn(crunch);
+
+        mvc.perform(get("/api/exercises/crunch")
         .contentType(MediaType.APPLICATION_JSON))
             .andExpect(status().isOk())
-            .andExpect(jsonPath("$.name", is("push-up")));
+            .andExpect(jsonPath("$.name", is("crunch")));
     }
 }
